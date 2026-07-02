0774f072def8ad280ed192f2e48ab2755eb1738d
Kripa Felix <kripafelixarthasseril@gmail.com>
2026-07-02 22:49:34 +0530

    Update lifo.v

--- lifo.v
+++ lifo.v
@@ -1,17 +1,22 @@
 `timescale 1ns/1ps
 
+// LIFO (stack) memory - 32 locations, 32 bits wide
+// last value pushed in is the first one popped out
+// sp = stack pointer, points to the next free slot
+
 module LIFO(data_out, full, empty, clk, rst, rw, en, data_in);
  
   input [31:0] data_in;
-  input clk, rst, rw, en; 
+  input clk, rst, rw, en;   // rw = 1 -> write/push, rw = 0 -> read/pop
   
   output [31:0] data_out;
   output full, empty;
   
   reg [31:0] data_out;
-  reg [5:0] sp;
-  reg [31:0] mem [31:0];
+  reg [5:0] sp;              // 6 bits so it can count up to 32 (need to hit 100000 for full)
+  reg [31:0] mem [31:0];     // the actual stack, 32 words
   
+  // full when sp has counted all the way up to 32, empty when sp is back to 0
   assign full = (sp == 6'b100000) ? 1 : 0;
   assign empty = (sp == 6'b000000) ? 1 : 0;
   
@@ -19,6 +24,8 @@ module LIFO(data_out, full, empty, clk, rst, rw, en, data_in);
   begin
     if (rst)
       begin
+        // clearing every location on reset, sp starts at 1 not 0
+        // (index 0 is not used for data, first push goes to mem[1])
         mem[0] <= 0;mem[1] <= 0;mem[2] <= 0;mem[3] <= 0;
         mem[4] <= 0;mem[5] <= 0;mem[6] <= 0;mem[7] <= 0;
         mem[8] <= 0;mem[9] <= 0;mem[10] <= 0;mem[11] <= 0;
@@ -31,11 +38,14 @@ module LIFO(data_out, full, empty, clk, rst, rw, en, data_in);
       end
     else if (en & rw & !full)
       begin
+        // push: write data at current sp, then move sp up
         mem[sp] <= data_in;
         sp <= sp + 1;
       end
     else if (en & !rw & !empty)
       begin
+        // pop: move sp down first, then read from that location
+        // (this is why last value written is the first one read back)
         sp <= sp - 1;
         data_out <= mem[sp];
       end
